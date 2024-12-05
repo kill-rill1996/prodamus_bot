142167fbdf2acca0704fe815e0bc0c8c767cb4ba
Alexandr Andreev <aleksandrandreev@MacBook-Pro-Aleksandr.local>
2024-12-05 16:13:16 +0300

    Add testing rep

---
+++ testing_requests/main.py
@@ -0,0 +1,22 @@
+import requests
+
+
+def main():
+    link_form = "https://sheva-nutrition.payform.ru/"
+
+    data = {
+        "order_id": 714371204,
+        "subscription": 2093463,
+        "customer_extra": "Информация об оплачиваемой подписке",
+        "do": "link",
+        "sys": ""
+    }
+
+    response = requests.get(link_form, params=data)
+    print(response)
+    payment_link = response.content.decode()
+    print(payment_link)
+
+
+if __name__ == '__main__':
+    main()
